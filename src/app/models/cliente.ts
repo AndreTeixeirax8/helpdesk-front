467a592f62b2a59da9300182bcad54af3393418f
André Teixeira <andre-x8@hotmail.com>
2022-06-13 20:34:08 -0300

    Crud do cliente

---
+++ src/app/models/cliente.ts
@@ -0,0 +1,10 @@
+export interface Cliente{
+    id?:any; //com o simbolo de ? pode ou não receber um id 
+    nome: string;
+    cpf: string;
+    email:string;
+    senha:string;
+    perfis:string[];
+    dataCriacao:any;
+
+}
